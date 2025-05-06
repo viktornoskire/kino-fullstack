a263e2175d5cd62779a8db2a51c6a1ad03e80ac0
Rikard Engström <rikardengstrom89@gmail.com>
2025-05-06 15:24:07 +0200

    fix typescript

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,17 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const url =
-  process.env.MONGODB_URI ||
-  "mongodb://localhost:27017/your_database_name";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/your_database_name";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(url),
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      };
-      console.log("MongoDB connected");
+    await mongoose.connect(url);
+
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("Error to connect to Mongodb:", error);
     process.exit(1);
