85c945237d16a48c45f350ae7f61b5ca1417addd
JohnnyAstrom <johnny.astrom@hotmail.com>
2025-05-09 17:43:06 +0200

    Added the three different variants of buttons according to the figma design with minor css changes of primary button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,32 @@
 import { FC } from "react";
 import { buttonType } from "@/types/Buttontypes";
 
-const Button: FC<buttonType> = ({ children, type, style, onClick, className }) => {
+interface ExtendedButtonType extends buttonType {
+  variant?: "primary" | "secondary" | "cancel";
+}
+
+const Button: FC<ExtendedButtonType> = ({
+  children,
+  type,
+  style,
+  onClick,
+  variant = "primary",
+}) => {
+  const baseClasses = "rounded-3xl text-xl font-bold pt-2 pb-2 pl-12 pr-12 text-[color:var(--color-kino-white)] cursor-pointer transition-colors duration-200";
+  
+  const variantClasses =
+  variant === "primary"
+    ? "bg-[color:var(--color-kino-darkred)] text-[color:var(--color-kino-white)] border border-[color:var(--color-kino-darkred)] hover:bg-[color:var(--color-kino-red)] hover:border-[color:var(--color-kino-red)]"
+    : variant === "secondary"
+    ? "bg-transparent text-[color:var(--color-kino-darkred)] border border-[color:var(--color-kino-darkred)] hover:bg-[color:var(--color-kino-red)] hover:text-[color:var(--color-kino-white)] hover:border-[color:var(--color-kino-red)]"
+    : "bg-transparent text-[color:var(--color-kino-white)] hover:opacity-80";
+
   return (
     <button
       type={type}
       style={style}
       onClick={onClick}
-      className={`${className} bg-kino-red rounded-4xl text-l font-bold pt-2 pb-2 pl-12 pr-12`}
+      className={`${baseClasses} ${variantClasses}`}
     >
       {children}
     </button>
