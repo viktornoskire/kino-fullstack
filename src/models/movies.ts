08ad95a4143c6863e5b7d26aef30066910bd61e1
Louise <emmalouisekarlsson@hotmail.com>
2025-05-09 16:28:01 +0200

    GET  movies from database

---
+++ src/models/movies.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+  _id: String,
+  title: String,
+  description: String,
+  genre: [String],
+  durationMinutes: Number,
+  posterUrl: String,
+  premiereDate: Date,
+  tags: [String],
+  ageLimit: Number,
+  ageRatingText: String,
+  slug: String,
+});
+
+const movie =
+  mongoose.models.movieResult || mongoose.model("movie", movieSchema);
+
+export { movie };
