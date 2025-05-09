6aefbd66b25a6618d02dfe8c10c926f1b16666b5
Emilyempa <emilypettersson@hotmail.com>
2025-05-09 14:03:59 +0200

    Update Button component to accept className prop and adjust styling Refactor globals.css to use @theme for CSS variables Add Footer component to RootLayout for consistent page structure

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,13 @@
 import { FC } from "react";
 import { buttonType } from "@/types/Buttontypes";
 
-const Button: FC<buttonType> = ({ children, type, style, onClick }) => {
+const Button: FC<buttonType> = ({ children, type, style, onClick, className }) => {
   return (
     <button
       type={type}
       style={style}
       onClick={onClick}
-      className="bg-kino-red rounded-4xl text-xl font-bold pt-2 pb-2 pl-12 pr-12"
+      className={`${className} bg-kino-red rounded-4xl text-l font-bold pt-2 pb-2 pl-12 pr-12`}
     >
       {children}
     </button>
