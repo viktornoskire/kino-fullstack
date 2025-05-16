a7d50a47465310e89f8ae8eefb1042ce59e35545
Louise <emmalouisekarlsson@hotmail.com>
2025-05-16 09:50:18 +0200

    Callback session to acces userinfo

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,16 +16,45 @@ export const authOptions: NextAuthOptions = {
       async authorize(credentials) {
         await connectDB();
 
-        const user = await User.findOne({ email: credentials?.email }).select('+password');
+        const user = await User.findOne({ email: credentials?.email }).select(
+          '+password'
+        );
 
         if (!user) throw new Error('Email invalid!');
 
-        const passwordMatch = await bcrypt.compare(credentials!.password, user.password);
+        const passwordMatch = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        );
 
         if (!passwordMatch) throw new Error('Password incorrect!');
         return user;
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log('jwt', { token, user });
+      //Here you can pass in id and phonnumber info
+      if (user) {
+        return { ...token, id: user.id };
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      console.log('session', { session, token });
+      //Pass in user id and phonnumber to session
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          name: token.name,
+        },
+      };
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: 'jwt' },
 };
