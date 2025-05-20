dcc1cef6eeb7f5438b921fdbfd15d9fa392a301b
ViktorNoskire <vikkerinho@gmail.com>
2025-05-20 10:06:20 +0200

    Removed folder test-error

--- src/app/test-error/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function ErrorTestPage() {
-  throw new Error("This is a test error. The error message will show here in real error");
-  return <div>You will never see this</div>;
-}
