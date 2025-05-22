ced82ec45835ece89c9b07c2a8b591a30c5fdeb0
Louise <emmalouisekarlsson@hotmail.com>
2025-05-22 15:45:39 +0200

    Icon button changes from + to - in InfoModal when opens/closes text field. Language fixes and minor changes.

--- src/types/Buttontypes.ts
+++ src/types/Buttontypes.ts
@@ -1,9 +1,10 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 export type buttonType = {
   children: ReactNode;
-  type: "button" | "reset" | "submit" | undefined;
+  type: 'button' | 'reset' | 'submit' | undefined;
   style?: React.CSSProperties;
+  open?: boolean;
   onClick?: () => void;
   className?: string;
 };
