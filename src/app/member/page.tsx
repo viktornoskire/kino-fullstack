34d88d0de8347489ff9e66002ed4f5fdbaae367a
Louise <emmalouisekarlsson@hotmail.com>
2025-05-22 16:18:21 +0200

    Made adjustments to display page for logged in member

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -9,11 +9,7 @@ export default function Member() {
   const { status } = session;
 
   if (status === 'authenticated') {
-    return (
-      <main className="max-w-6xl mx-auto pb-10 px-4 mt-4 md:px-6 lg:mt-10 text-white">
-        <InfoMeber />
-      </main>
-    );
+    return <InfoMeber />;
   } else if (status === 'unauthenticated') {
     return (
       <main className="max-w-6xl mx-auto pb-10 px-4 mt-4 md:px-6 lg:mt-10 text-white">
