c70a6b3e757c9f884988618f556f456213d5147d
ViktorNoskire <vikkerinho@gmail.com>
2025-06-01 12:55:22 +0200

    Image test

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'kino-fullstack-*.vercel.app',
+        hostname: 'kino-fullstack-git-main-viktornoskires-projects.vercel.app',
         pathname: '/**',
       },
     ],
